99ce787b72fb0753ed268723145c7a2e215bef41
James Moore <jamesmooredeveloper@gmail.com>
2022-05-17 21:49:50 +0100

    more  coverage

--- MP3InfoTest/ArtExport/NonDestructiveFileSaverTest.cs
+++ MP3InfoTest/ArtExport/NonDestructiveFileSaverTest.cs
@@ -61,5 +61,20 @@ namespace MP3InfoTest.ArtExport
             Assert.IsFalse(File.Exists("testfile1.txt"));
             File.Delete("testfile.txt");
         }
+
+        [TestMethod]
+        public void NonDestructiveFileSaver_Multiple_Different_File_Test()
+        {
+            File.WriteAllText("testfile.txt", "xyz7890", Encoding.ASCII);
+            File.WriteAllText("testfile1.txt", "xyz7890", Encoding.ASCII);
+
+            var sut = new NonDestructiveFileSaver();
+            sut.SaveBytesToFile(Encoding.ASCII.GetBytes("abc123"), (int? i) => $"testfile{i}.txt");
+
+            Assert.IsTrue(File.Exists("testfile2.txt"));
+            File.Delete("testfile.txt");
+            File.Delete("testfile1.txt");
+            File.Delete("testfile2.txt");
+        }
     }
 }
