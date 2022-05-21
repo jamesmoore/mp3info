aaf24d9836a1601c6f087c0b5b902b9b4bc4ecd8
James Moore <jamesmooredeveloper@gmail.com>
2022-05-21 17:32:50 +0100

    make test os independent

--- MP3InfoTest/Rename/TrackRenameTest.cs
+++ MP3InfoTest/Rename/TrackRenameTest.cs
@@ -21,8 +21,8 @@ namespace MP3InfoTest.Rename
         {
             const string Filename = "Musicks_Recreation_Milena_Cord-to-Krax_-_01_-_Prelude__Tres_viste_BWV_995.mp3";
 
-            const string testFileName = @"c:\temp\testfile.mp3";
-            const string testPictureName = @"C:\temp\albumcover.jpg";
+            string testFileName = @".\temp\testfile.mp3".ToCurrentSystemPathFormat();
+            string testPictureName = @".\temp\albumcover.jpg".ToCurrentSystemPathFormat();
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
             {
                 { testFileName, new MockFileData(File.ReadAllBytes(Filename)) },
@@ -40,9 +40,9 @@ namespace MP3InfoTest.Rename
 
             sut.ProcessTrack(track, ".");
 
-            Assert.AreEqual(expectFileMovedToDestination, fileSystem.File.Exists(expectedDestination));
+            Assert.AreEqual(expectFileMovedToDestination, fileSystem.File.Exists(expectedDestination.ToCurrentSystemPathFormat()));
             Assert.AreEqual(expectFileMovedToDestination, fileSystem.File.Exists(fileSystem.Path.Combine( 
-                fileSystem.FileInfo.FromFileName(expectedDestination).DirectoryName,
+                fileSystem.FileInfo.FromFileName(expectedDestination.ToCurrentSystemPathFormat()).DirectoryName,
                 "albumcover.jpg"
                 )));
 
